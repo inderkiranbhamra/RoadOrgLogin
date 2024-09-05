f992061c81b877b0ebc446ba9f7854cf76afb321
inderkiranbhamra <140848814+inderkiranbhamra@users.noreply.github.com>
2024-09-05 23:30:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 import mysql.connector
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # MySQL Database Configuration
 DB_HOST = '217.21.94.103'
